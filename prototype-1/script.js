bc8d0739fb398865acc6b4faef93148b4a967574
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2021-12-09 11:51:34 +0100

    added

--- prototype-1/script.js
+++ prototype-1/script.js
@@ -1,44 +1,41 @@
 document.getElementById("formSubmit").addEventListener("submit", function (event) {
     event.preventDefault();
     var work = readwork();
-    insertNewRow(ouvrage);
+    insertNewRow(work);
 })
 
 
-function lireouvrage() {
+function readwork() {
 
-    var ouvrage = {};
-    ouvrage["titre"] = document.getElementById("titreEntrée").value;
-    ouvrage["auteur"] = document.getElementById("auteurEntrée").value;
-    ouvrage["prix"] = parseFloat(document.getElementById("prixEntrée").value);
-    ouvrage["date"] = document.getElementById("dateEntrée").value;
-    ouvrage["langue"] = document.getElementById("langueEntrée").value;
-    var cheackValues = document.getElementsByName("ouvrageType");
+    var work = {};
+    work["title"] = document.getElementById("inputTitle").value;
+    work["author"] = document.getElementById("inputAuthor").value;
+    work["price"] = parseFloat(document.getElementById("inputPrix").value);
+    work["date"] = document.getElementById("inputDate").value;
+    work["language"] = document.getElementById("inputLanguage").value;
+    var cheackValues = document.getElementsByName("workType");
     for (var i = 0; i < cheackValues.length; i++) {
         if (cheackValues[i].checked) {
-            ouvrage["type"] = cheackValues[i].value;
+            work["type"] = cheackValues[i].value;
             break;
         }
     }
-    return ouvrage;
+    return work;
 }
 
 
 
-function insertNewRow(ouvrage) {
-    var tableBody = document.getElementById("ouvragesTable").getElementsByTagName('tbody')[0];
+function insertNewRow(work) {
+    var tableBody = document.getElementById("worksTable").getElementsByTagName('tbody')[0];
     var newRow = tableBody.insertRow(tableBody.length);
-    newRow.insertCell(0).innerHTML = ouvrage.title;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = ouvrage.author;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = ouvrage.price;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = ouvrage.date;
+    newRow.insertCell(0).innerHTML = work.title;
+    newRow.insertCell(1).innerHTML = work.author;
+    newRow.insertCell(2).innerHTML = work.price;
+    newRow.insertCell(3).innerHTML = work.date;
     cell5 = newRow.insertCell(4);
-    cell5.innerHTML = ouvrage.language
+    cell5.innerHTML = work.language
     cell6 = newRow.insertCell(5)
-    cell6.innerHTML = ouvrage.type
+    cell6.innerHTML = work.type
     cell7 = newRow.insertCell(6)
 
 
