2260db1e8d90d6ef82c40c4ada0816a1bc0763dd
elyahyaoui-oubay <elyahyaoui.oubay@gmail.com>
2022-01-10 02:10:13 +0100

    update

--- prototype-1/workBLO.js
+++ prototype-1/workBLO.js
@@ -1,16 +1,46 @@
 class WorkBLO {
-    workList;
-    counter = 0;
+    #workList = [];
+    #counter = 0;
+
+    get workList(){
+        return this.#workList;
+    }
 
     addWork(work) {
-        this.workList.push(work)
+        this.#counter = this.#counter + 1
+        work.id = this.#counter
+        this.#workList.push(work)
     }
 
     editWork(work) {
-      for(var i =0; i< this.workList.length; i++){
-          if(work.id == this.workList[i]){
-              this.workList[i] = work
-          }
-      }  
+        console.log(work)
+        for (var i = 0; i < this.#workList.length; i++) {
+            if (work.id == this.#workList[i].id) {
+                this.#workList[i] = work
+                return work
+            }
+        }
+    }
+
+
+    getItem(id) {
+        for (var i = 0; i < this.workList.length; i++) {
+            if (id == this.workList[i].id) {
+                return this.workList[i]
+            }
+        }
+    }
+
+    deleteWork(id) {
+       this.#workList =  this.#workList.filter(function(work){
+            return work.id != id
+        })
+    }
+
+
+    getAllItems() {
+        return this.#workList.sort(function(a, b) {
+            return a.title.localeCompare(b.title)
+        })
     }
 }
